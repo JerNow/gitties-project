2ab4afa2a9a986c0b77b4e62a76c264b00295a42
Jerzy Noworolnik <jnoworolnik@gmail.com>
2022-04-20 10:34:24 +0200

    Overwriting Gittie.cs shout

--- Gittie.cs
+++ Gittie.cs
@@ -11,9 +11,13 @@ public class Gittie
         Console.WriteLine("Funny joke here");
     }
     public void Add(int a, int b);
+<<<<<<< Updated upstream
 
     public void Shout()
     {
         Console.WriteLine("Alelelele");
     }
+=======
+    public void Shout(); //shout person A
+>>>>>>> Stashed changes
 }
