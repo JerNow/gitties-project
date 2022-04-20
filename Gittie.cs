b334bab0f1088045f10c7abb96da04eb2673540c
Setsumiku <99953781+Setsumiku@users.noreply.github.com>
2022-04-20 10:12:51 +0200

    Change Introduce method

--- Gittie.cs
+++ Gittie.cs
@@ -4,7 +4,7 @@ public class Gittie
 {
     public void Introduce()
     {
-        Console.WriteLine("Hello, I'm Gittie!");
+        Console.WriteLine("Hello, I'm big Gittie!");
     }
     public void Joke()
     {
